cdbf48f1a4838b17e52f9a9a2d8c192485c64275
19di32qa <87597749+19di32qa@users.noreply.github.com>
2022-11-27 17:18:02 +0200

    "Test commit ide"

--- src/test/java/OrangeHPM/PageObjects/Job.java
+++ src/test/java/OrangeHPM/PageObjects/Job.java
@@ -1,4 +1,11 @@
 package OrangeHPM.PageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class Job {
+    WebDriver driver;
+    public  Job(WebDriver driver) {
+        this.driver = driver;
+    }
 }
